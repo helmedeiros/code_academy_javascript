b1e4dba2f96f842d50408622eb01bd3e7330a9a3
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-27 22:59:07 -0300

    first exercise done.

---
+++ meetTheFunctions/1_5-remenberConditionals.js
@@ -0,0 +1,9 @@
+var balance = 20.97;
+
+// Complete the condition in the ()s on line 4
+if (balance < 10 ) {
+	console.log(balance - 5);// console.log() the balance minus 5 dollars
+	
+} else {
+	console.log(balance); // Just console.log() the balance
+}
