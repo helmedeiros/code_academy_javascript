0e04c0ca85ef54c779313b59600ca46e5f5a2bbc
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-27 23:49:52 -0300

    fixed to get true return

--- functionsAndReturn/3_3-functionReturnIfElse.js
+++ functionsAndReturn/3_3-functionReturnIfElse.js
@@ -1,10 +1,9 @@
 // Define quarter here.
+var quarter = function(number){
+    return number / 4;  
+};
 
-
-
-
-
-if (quarter() % 3 === 0 ) {
+if (quarter(24) % 3 === 0 ) {
   console.log("The statement is true");
 } else {
   console.log("The statement is false");
