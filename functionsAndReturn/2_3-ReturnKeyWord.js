de571627377ceed8bcdf8579d64202ec3f1be51d
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-27 23:45:38 -0300

    A function can also return a value.

---
+++ functionsAndReturn/2_3-ReturnKeyWord.js
@@ -0,0 +1,8 @@
+// Parameter is a number, and we do math with that parameter
+var timesTwo = function(number) {
+    return number * 2;
+};
+
+// Call timesTwo here!
+var newNumber = timesTwo(4);
+console.log(newNumber);
