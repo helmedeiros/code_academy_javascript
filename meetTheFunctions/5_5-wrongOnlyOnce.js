ca888f2fd512b08b3ac12525a16234b518e42f3a
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-27 23:23:47 -0300

    First wrong code

---
+++ meetTheFunctions/5_5-wrongOnlyOnce.js
@@ -0,0 +1,9 @@
+// Nicely written function:
+var calculate = function (number) {
+    var val = number * 10;
+    console.log(val);
+};
+
+// Badly written function with syntax errors!
+
+greeting var func{name}(console.log(name)))}
