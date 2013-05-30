7ffa9cc68384ffe3bb7b8931661d45382172a817
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-30 12:29:38 -0300

    declared variables

---
+++ SearchTextForYourName/2_7-DeclareYourVariables.js
@@ -0,0 +1,25 @@
+/*jshint multistr:true 
+Declare your variables
+First, we'll start by declaring the variables we'll be using: text, myName, and hits.
+
+Since text could be quite long, try putting a backslash (\) at the end of each line to make your string "wrap" to the next line of the editor. That way you avoid really long lines! (Feel free to peek back at the first exercise for an example.)
+
+You can ignore the /*jshint... line for now. All that does is tell the console to stop worrying about our use of backslash characters for wrapping long lines of text.
+
+INSTRUCTIONS
+Declare and set the following three variables:
+text, and make it a string containing some text. Place your name in there a couple of times!
+myName, and make it a string containing just your name.
+hits, and make it an empty array.
+*/
+
+/*jshint multistr:true */
+var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus cursus, turpis at sodal \
+, nulla dui egestas Helio urna, ut ultrices ligula elit semper magna. Vestibulum vitae eros turpis. Donec \
+faucibus sodales dolor, id congue dolor tincidunt sed. Pellentesque ac nisi non erat ullamcorper \
+sodales elementum id leo. Helio integer non dictum dui. Nulla facilisi. Curabitur facilisis, ullamcorper \
+iaculis euismod, ante lorem egestas justo, eu imperdiet quam orci vitae nisi. Ut Helio tellus ut quam \
+bibendum aliquam. Etiam ullamcorper, ante a euismod mattis, libero tellus vehicula tellus, a cursus \
+tortor augue vel orci. Phasellus at vestibulum lectus.";
+var myName = "Helio";
+var hits = [];
