0aa40fcca2628fe050508db48f768ffb519c8b56
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-30 10:01:02 -0300

    resolved problem, reforced counting knowledge

--- practicingLoops/1_3-HowItWorks.js
+++ practicingLoops/1_3-HowItWorks.js
@@ -9,7 +9,14 @@ Here, the code block prints out the value of i. It is currently 2 so 2 will be p
 Once the code block is finished, the for loop then increments / decrements. Here, we add 1.
 Now i = 3. We check if it is less than 13. If it is true, we run the code block.
 The code block runs, and then we increment.
-We repeat these steps until the condition i < 13 is no longer true.*/
-for (var i = 2 ; i < 13; i++) {
+We repeat these steps until the condition i < 13 is no longer true.
+
+INSTRUCTIONS
+1.Make the computer start counting at 8.
+2.Keep counting while i < 120.
+3.Count up by increments of 12.
+Remember, if you accidentally make a loop that never ends, your browser will crash!
+*/
+for (var i = 8 ; i < 120; i+=12) {
 	console.log(i);
 }
