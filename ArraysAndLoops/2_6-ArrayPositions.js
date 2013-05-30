83e22cd687db7b443f79f937dd4d9cfb6bce4b50
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-30 10:49:52 -0300

    Learning about array positions

---
+++ ArraysAndLoops/2_6-ArrayPositions.js
@@ -0,0 +1,15 @@
+// Practice array!
+/*Array positions
+It's nice that we can put a list of data into an array. But now we need to learn how to get access to the data inside the array.
+
+The position of things in arrays is fixed. So we just need to know the array name (here, it is junkData), and the position of the data we want, and we're done.
+
+Small complication: the position (or the index) of each bit of data is counted starting from 0, not 1.
+
+First element in the array: junkData[0]
+Third element in the array: junkData[2]
+Arrays have 0-based indexing, so we start counting the positions from 0.
+*/
+
+var junkData = ["Eddie Murphy", 49, "peanuts", 31];
+
