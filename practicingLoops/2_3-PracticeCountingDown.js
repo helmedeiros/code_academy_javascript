1383c1ff75f40ab1079c230a969d76bae8fe0648
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-30 10:09:55 -0300

    solved the counting down; starting in 10, stopping in 0 and counting down by 1

--- practicingLoops/2_3-PracticeCountingDown.js
+++ practicingLoops/2_3-PracticeCountingDown.js
@@ -13,7 +13,13 @@ c. Because now i = 1, the code will run.
 
 d. We increment i by 1 and now i = 2. This satisfies the condition. We run the code. 
 e. Increment i by 1 and now i = 3. This satisfies the condition that i >= 1. We run the code.
-f. We will keep incrementing the code up by 1. It will always satisfy the condition. The loop NEVER ends. This will crash your computer!*/
-for (var i = 1; i >= 1; i++) {
+f. We will keep incrementing the code up by 1. It will always satisfy the condition. The loop NEVER ends. This will crash your computer!
+
+INSTRUCTIONS
+1.Change this code such that it starts counting from 10.
+2.We want it to stop counting at 0.
+3.We want it to count down by 1.
+*/
+for (var i = 10; i >= 0; i--) {
 	console.log(i);
 }
