cd0cbf2ba38fab7ab0cfc1696fcbfe4c598d7e30
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-30 10:38:07 -0300

    initial array problem.

---
+++ ArraysAndLoops/1_6-MeetArrays.js
@@ -0,0 +1,21 @@
+// You are now declaring an array.
+// Arrays are an awesome data structure!
+/*Meet arrays
+Variables can store numbers or strings. But so far, we've only been able to store ONE number or ONE string. Good thing we have arrays. Arrays:
+
+a. store lists of data
+b. can store different data types at the same time
+c. are ordered so the position of each piece of data is fixed
+
+Example:
+
+var names = ["Mao","Gandhi","Mandela"];
+
+var sizes = [4, 6, 3, 2, 1, 9];
+
+var mixed = [34, "candy", "blue", 11];
+Syntax:
+var arrayName = [data, data, data];
+
+Any time you see data surrounded by [ ], it is an array.
+*/
