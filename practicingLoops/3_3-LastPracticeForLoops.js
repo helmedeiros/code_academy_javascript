b3e163ae1da91ce3e5062b56fc59be0c920f3383
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2013-05-30 10:19:24 -0300

    counting down last exercise.

---
+++ practicingLoops/3_3-LastPracticeForLoops.js
@@ -0,0 +1,12 @@
+// Write your very own for loop!
+/*Last practice for loop
+You have a great handle on for loops now! This will be the last practice one before we look at cool ways to use them.
+
+The next exercise introduces you to arrays. So instead of just counting numbers up and down, we can make the computer do many more interesting things with loops.
+
+INSTRUCTIONS
+Once more, for practice: write a for loop that gets the computer to count down from 100 to 0 by 5.
+*/
+for(var i = 100; i > 0; i-=5 ){
+	console.log(i);
+}
